8042b7f9ed3a7f6ba94316c2afaa24f7015c0670
martin.bryndza <bryndza.martin@gmail.com>
2013-10-12 00:12:39 +0200

    Added DAO classes for Activity entity

---
+++ CaloryCounter-Backend/src/main/java/cz/fi/muni/pa165/calorycounter/backend/dao/ActivityDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pa165.calorycounter.backend.dao;
+
+import cz.fi.muni.pa165.calorycounter.backend.model.Activity;
+
+/**
+ *
+ * @author Martin Bryndza (martin.bryndza)
+ */
+public interface ActivityDao extends Dao<Activity> {
+}
