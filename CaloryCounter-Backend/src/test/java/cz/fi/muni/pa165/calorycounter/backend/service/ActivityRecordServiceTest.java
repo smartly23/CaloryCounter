d99f668fd56c611dd84c98d06da7f8d63c541b8e
smartly23 <mt.pasko@gmail.com>
2013-10-29 11:45:06 +0100

    added option to choose embedded DB

--- CaloryCounter-Backend/src/test/java/cz/fi/muni/pa165/calorycounter/backend/service/ActivityRecordServiceTest.java
+++ CaloryCounter-Backend/src/test/java/cz/fi/muni/pa165/calorycounter/backend/service/ActivityRecordServiceTest.java
@@ -1,8 +1,10 @@
 package cz.fi.muni.pa165.calorycounter.backend.service;
 
+import cz.fi.muni.pa165.calorycounter.backend.dao.UserDao;
 import cz.fi.muni.pa165.calorycounter.backend.dto.ActivityRecordDto;
 import cz.fi.muni.pa165.calorycounter.backend.model.AuthUser;
 import cz.fi.muni.pa165.calorycounter.backend.model.WeightCategory;
+import cz.fi.muni.pa165.calorycounter.backend.service.impl.ActivityRecordServiceImpl;
 import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,34 +21,32 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Propagation;
 
 /**
  * Unit Tests using Mockito to mock DAO objects.
  *
  * @author Martin Pasko (smartly23)
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-    "classpath:applicationContext.xml"})
 @TransactionConfiguration(defaultRollback = true)
 @Transactional
 public class ActivityRecordServiceTest {
 
     private static AuthUser user;
     private static ActivityRecordService activityRecordService;
-    @PersistenceContext
-    private EntityManager em;
-    
+    private static UserDao userDao;
+
     @BeforeClass
     public static void setUpOnce() {
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
         activityRecordService = (ActivityRecordService) context.getBean("activityRecordService");
+        userDao = (UserDao) context.getBean("userDaoJPA");
     }
 
     @AfterClass
     public static void tearDownOnce() {
+        activityRecordService = null;
+        userDao = null;
     }
 
     @Before
@@ -56,14 +56,17 @@ public class ActivityRecordServiceTest {
         user.setGender("female");
         user.setName("Edita Papeky");
         user.setWeightCat(WeightCategory._180_);
-        em.persist(user);
+        user.setUsername("Petra");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("userDao: " + userDao);
+        System.out.println("user: " + user);
+        userDao.create(user);
     }
 
     @After
     public void tearDown() {
-        //if (em != null) {
-        //    em.close();
-        //}
         user = null;
     }
 
@@ -76,27 +79,27 @@ public class ActivityRecordServiceTest {
         dto.setDuration(40);
         dto.setCaloriesBurnt(1000);
         dto.setWeightCatNum(2);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("user id: " + user.getId());
         dto.setUserId(user.getId());
-        
+
         // test create():
-        //Long id = activityRecordService.create(dto);
-        Long id = temp(dto);
-        assertNotNull(id);
-        System.out.println("Id is: "+id);
-    }
-    
-    @Test
-    public void testGet() {}
-    
-    @Test
-    public void testUpdate() {}
-    
-    @Test
-    public void testRemove() {}
-    
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    private Long temp(ActivityRecordDto dto) {
         Long id = activityRecordService.create(dto);
-        return id;
+        assertNotNull(id);
+        System.out.println("Id is: " + id);
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testRemove() {
     }
 }
